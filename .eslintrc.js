00fc51132b7a895f41f280d3a7b93f3e75d073f1
stercoris <dimarodin0@gmail.com>
2021-03-05 13:13:16 +0400

    Перенес изменения с компа на ноут((( забыл закоммитить

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "airbnb/base",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: [
-    "@typescript-eslint",
-  ],
-  rules: {
-    indent: "off",
-    "linebreak-style": 0,
-    quotes: ["error", "double"],
-    camelcase: "off",
-    "import/extensions": ["error", "never"],
-    "import/no-unresolved": "off",
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-    "no-useless-constructor": "off",
-    "no-async-promise-executor": "off",
-    "no-cond-assign": "off",
-    "valid-jsdoc": ["error", {
-      requireReturnType: false,
-      requireParamType: false,
-      requireParamDescription: false,
-    }],
-    "no-underscore-dangle": "off",
-  },
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "airbnb/base",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    rules: {
+        indent: "off",
+        "linebreak-style": 0,
+        quotes: ["error", "double"],
+        camelcase: "off",
+        "import/extensions": ["error", "never"],
+        "import/no-unresolved": "off",
+        "no-shadow": "off",
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-useless-constructor": "off",
+        "no-async-promise-executor": "off",
+        "no-cond-assign": "off",
+        "valid-jsdoc": ["error", {
+            requireReturnType: false,
+            requireParamType: false,
+            requireParamDescription: false,
+        }],
+        "no-underscore-dangle": "off",
+    },
 };
