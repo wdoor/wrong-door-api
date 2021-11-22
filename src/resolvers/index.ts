4aa9f1e45eb5997dff00ce93eedd1e8d1f0a4f19
Dmitriy Rodin <dimarodin0@gmail.com>
2021-11-22 23:54:44 +0400

    refactor(resolvers): add one point of export

---
+++ src/resolvers/index.ts
@@ -0,0 +1,4 @@
+export { ChatResolver } from "@Resolvers/chat/chatResolver";
+export { CommandResolver } from "@Resolvers/commands/commandsResolver";
+export { LogsResolver } from "@Resolvers/logs/logsResolver.";
+export { UserResolver } from "@Resolvers/user/userResolver";
